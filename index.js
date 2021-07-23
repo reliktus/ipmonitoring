f05bf18d1b0c5072874f1174832045d0a62d2e03
Rafal J <reliktus@gmail.com>
2021-07-23 14:54:12 +0200

    formate init

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-let ping = require ("net-ping");
-const sessionConfig ={
+let ping = require('net-ping')
+const sessionConfig = {
     // networkProtocol: ping.NetworkProtocol.IPv4,
     // packetSize: 16,
     // retries: 3,
     // sessionId: (process.pid % 65535),
     timeout: 2000,
     // ttl: 128
-};
-var session = ping.createSession(sessionConfig);
+}
+var session = ping.createSession(sessionConfig)
 
-const target = 'http://onet.pl';
+// const target = 'http://onet.pl';
+const target = '192.168.1.1'
 setInterval(() => {
-    pingAddress(target);
-}, 3000);
+    pingAddress(target)
+}, 3000)
 
 function pingAddress(target) {
-    session.pingHost (target, function (error, target) {
-            if (error)
-                console.log (target + ": " + error.toString ());
-            else
-                console.log (target + ": Alive");
-            });
-        }
+    session.pingHost(target, function (error, target) {
+        error
+            ? console.log(target + ': ' + error.toString())
+            : console.log(target + ': Alive')
+    })
+}
